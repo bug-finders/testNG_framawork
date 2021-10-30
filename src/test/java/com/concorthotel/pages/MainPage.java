79602a2f015949bac10f5d1ad9b07d626633ff2e
Emel614 <emelny614@gmail.com>
2021-10-30 16:01:32 +0200

    concort

--- src/test/java/com/concorthotel/pages/MainPage.java
+++ src/test/java/com/concorthotel/pages/MainPage.java
@@ -1,4 +1,14 @@
 package com.concorthotel.pages;
 
+import com.concorthotel.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class MainPage {
+    public MainPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    @FindBy(linkText = "Log in")
+    public WebElement loginLink;
 }
--- src/test/java/com/concorthotel/tests/US_002.java
+++ src/test/java/com/concorthotel/tests/US_002.java
@@ -1,8 +1,4 @@
 package com.concorthotel.tests;
 
 public class US_002 {
-    //emel
-    public static void main(String[] args) {
-        System.out.println();
-    }
 }
